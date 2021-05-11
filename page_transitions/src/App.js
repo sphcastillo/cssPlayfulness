96a0d2841801b6d9cbad8dbd0d9e00ab1a190b1c
Sophia Castillo <Sphcastillo@gmail.com>
2021-05-11 15:13:18 -0700

    created header/navbar

---
+++ page_transitions/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Router>
+          <Switch>
+              <Route path='/' component={Home}></Route>
+          </Switch>
+
+      </Router>
+
+    </>
+  )
+}
+
+export default App;
---
+++ page_transitions/src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <div>
+            <h2>hero</h2>
+        </div>
+    )
+}
+
+export default Hero;
